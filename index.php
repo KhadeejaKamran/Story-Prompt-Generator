63c088f87aace707ea5298d50e7d6af4766b17ff
Khadeeja Kamran <118957732+KhadeejaKamran@users.noreply.github.com>
2025-09-13 16:56:15 +0300

    Update index.php

--- index.php
+++ index.php
@@ -4,25 +4,26 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
+    <script defer src="script.js"></script>
     <title>Prompt Generator</title>
 </head>
 <body>
-    <div>
-        <div class="prompt-section">
-            <h2>-- Your Creative Writing Prompt --</h2>
-            <?php
-            $prompt = ["Write a story where a man from the 19th century time travels to 2025. Write about his reaction to the minorities now in leaderships.", 
-            "Write a scene where the villain bails out the wrongly imprisoned main character.", 
-            "Write a story about a book whose events are turning into reality.", 
-            "Write a scene where two strangers meet at a train station and realize they share the same recurring nightmare.", 
-            "Write a short story where the detectives who have been solving a murder case end up finding a clue to a mystery that had been left unsolved 10 years ago.", 
-            "Write a scene where a flower narrates its own journey as it lies on a pile of garbage.", 
-            "Write the starting scene of a story where the main character wakes up to find out that they're living their dream life."];
-
-            echo "<p class='prompt'>". $prompt[array_rand($prompt)] ."</p>";
-            ?>
+ <div class="main-page">
+    <div class="title">Random Prompt Generator</div>
+    <nav class="genre-nav">
+       <button onclick="generatePrompt('fantasy')">Fantasy</button>
+       <button onclick="generatePrompt('sci-fi')">Sci-Fi</button>
+       <button onclick="generatePrompt('mystery')">Mystery</button>
+       <button onclick="generatePrompt('romance')">Romance</button>
+       <button onclick="generatePrompt('thriller')">Thriller</button>
+       <button onclick="generatePrompt('non-fiction')">Non-fiction</button>
+    </nav>
+    <div class="prompt-section">
+        <h2>Your Creative Writing Prompt</h2>
+        <div class="prompt-display"></div>
         </div>
     </div>
     
 </body>
 </html>
+
