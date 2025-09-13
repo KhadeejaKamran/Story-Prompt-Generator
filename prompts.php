f5910400af53289d722ded09b87866ed898ef879
Khadeeja Kamran <118957732+KhadeejaKamran@users.noreply.github.com>
2025-09-13 17:05:15 +0300

    Add files via upload

---
+++ prompts.php
@@ -0,0 +1,30 @@
+<?php $prompts = [
+'romance' => 
+["Write a short story where one character acts as a love cupid for a couple and ends up finding their own soulmate by the end.",
+ "Write a scene where the main character realizes the person they hated the most was actually their soulmate.",
+ "Write a scene where "],
+
+'fantasy' => 
+["Write a story about a book whose events are turning into reality.",
+"Write a scene where a flower narrates its own journey as it lies on a pile of garbage.",
+"Write the starting scene of a story where the main character wakes up to find out that they're living their dream life."],
+
+'thriller' => 
+["Write a scene where the villain bails out the wrongly imprisoned main character.",
+"Write a scene where a psychologist finds out that the patient was pretending to be mentally ill, after many years.",
+"Write a short story about the hidden secret of so-called orphanages, hiding behind a giant lobby of child labor."],
+
+'mystery' => ["Write a scene where two strangers meet at a train station and realize they share the same recurring nightmare.",
+"Write a short story where the detectives who have been solving a murder case end up finding a clue to a mystery that had been left unsolved 10 years ago.",
+"Write a scene where the main character comes face to face with a friend they thought died years ago."],
+
+'sci-fi' => ["Write a story where a man from the 19th century time travels to 2025. Write about his reaction to the minorities now in leaderships.",
+"Write a scene of a world where robots are chefs.",
+"Write a short story about the possible end of the world as robots take over."],
+
+'non-fiction' => 
+["Write the incomprehensible reality of how humans managed to capture a moment in time through cameras.",
+"Write about ways we can manipulate our brain to turn boring tasks into interesting ones.",
+"Analyze how the mind of a narcissist works."]
+];
+    ?>
